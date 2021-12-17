341e424cf6585e70a96c17845f7b46ff80d06f60
Felipe Volpato <codeduck@Mac-mini-de-Felipe.local>
2021-12-16 22:23:42 -0300

    Base live 1

--- www/views/add.php
+++ www/views/add.php
@@ -2,29 +2,29 @@
 
 <form action="" method="post">
     <div class="form-group">
-        <label for="name">Nome</label>
+        <label for="name">Nome do Produto</label>
         <input type="text" name="name" id="name" class="form-control" />
     </div>
     <div class="form-group">
-        <label for="description">Descrição</label>
-        <textarea name="description" id="description" class="form-control"></textarea>
-    </div>
-    <div class="form-group">
-        <label for="price">Preço</label>
+        <label for="price">Preço Venda</label>
         <input type="text" name="price" id="price" class="form-control" />
     </div>
     <div class="form-group">
-        <label for="category">Categoria</label>
-        <select name="category" id="category" class="form-control">
-            <option value="">Selecione</option>
-            <?php foreach ($categories as $category): ?>
-                <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
-            <?php endforeach; ?>
-        </select>
+        <label for="price">Custo</label>
+        <input type="text" name="price" id="price" class="form-control" />
     </div>
     <div class="form-group">
-        <label for="image">Imagem</label>
-        <input type="file" name="image" id="image" class="form-control" />
+        <label for="price">Quantidade</label>
+        <input type="text" name="price" id="price" class="form-control" />
     </div>
-    <input type="submit" value="Adicionar" class="btn btn-primary" />
+    <div class="form-group">
+        <label for="price">Custo</label>
+        <input type="text" name="price" id="price" class="form-control" />
+    </div>
+    <div class="form-group">
+        <label for="price">Quantidade Mínima</label>
+        <input type="text" name="price" id="price" class="form-control" />
+    </div>
+
+    <button type="submit" class="btn btn-primary">Adicionar</button>
 </form>
